a790f8c8d80149d92572f890db2cde0fdfe19010
lifrank1 <frank.li.865985@gmail.com>
2025-08-24 14:55:13 -0400

    red dom change

--- code.js
+++ code.js
@@ -1,3 +1,26 @@
 function add7(string) {
     return string.charAt((string.length)-1);
 }
+
+function writeMap(array) {
+    let newArray = array.map((element) => element+1)
+    return newArray
+}
+
+function sumOfTripledEvens(array) {
+    let constArray = array.filter((element) => {return element % 2 === 0})
+    console.log(constArray)
+    let tripleArray = constArray.map((element) => {return element * 3})
+    console.log(tripleArray)
+    let sum = tripleArray.reduce((total, element) => {return total += element}, 0)
+    console.log(sum)
+    return sum
+}
+
+let container = document.querySelector("#identity")
+
+let child = document.createElement("p")
+child.style.color = "red"
+child.textContent = "Hey I'm red"
+
+container.appendChild(child)
